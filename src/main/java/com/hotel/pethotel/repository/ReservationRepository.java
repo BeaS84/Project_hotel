e469594c96bde6069555f4e175f7c592c904e1b0
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-12-07 17:31:48 +0100

    show all reservations adminpanel

--- src/main/java/com/hotel/pethotel/repository/ReservationRepository.java
+++ src/main/java/com/hotel/pethotel/repository/ReservationRepository.java
@@ -1,9 +1,13 @@
 package com.hotel.pethotel.repository;
 
+import com.hotel.pethotel.model.AnimalModel;
 import com.hotel.pethotel.model.ReservationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+import java.util.Optional;
+
 public interface ReservationRepository extends JpaRepository<ReservationModel, Long> {
+    Optional<ReservationModel> findById(Long id);
 }
