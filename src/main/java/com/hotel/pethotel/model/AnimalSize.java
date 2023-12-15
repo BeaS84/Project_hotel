d57355e2909f94dc77c3e4334bd2a535ec5ae537
BeaS84 <szydlowska.beata+wit@gmail.com>
2023-12-15 09:44:54 +0100

    Animal flow -added labels to enums - user panel

--- src/main/java/com/hotel/pethotel/model/AnimalSize.java
+++ src/main/java/com/hotel/pethotel/model/AnimalSize.java
@@ -1,9 +1,9 @@
 package com.hotel.pethotel.model;
 
 public enum AnimalSize {
-    SMALL("MAŁY (Pies <= 10kg | Kot <= 3kg)"),
-    MEDIUM("ŚREDNI(Pies 11 - 25kg | Kot 4 - 6kg)"),
-    LARGE ("DUŻY(Pies > 25kg | Kot > 6 kg)");
+    SMALL("Mały (Pies <= 10kg | Kot <= 3kg)"),
+    MEDIUM("Średni (Pies 11 - 25kg | Kot 4 - 6kg)"),
+    LARGE ("Duży (Pies > 25kg | Kot > 6 kg)");
 
     public final String label;
     private AnimalSize(String label){
